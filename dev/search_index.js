dfaf964f2a9699444a413fc6b01bb99fc226da77
Documenter.jl <documenter@juliadocs.github.io>
2025-05-05 23:54:49 +0000

    build based on 5b5ed65

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CNNSeq2exp","page":"Home","title":"CNNSeq2exp","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CNNSeq2exp.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
